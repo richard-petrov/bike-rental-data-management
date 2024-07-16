d4be9f87d385914b8e8783e3acf31a721ea72313
Richard Petrov <rihard.petroff@gmail.com>
2024-07-16 12:28:50 +0400

    pipenv_and_main_file

---
+++ main.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import pandas as pd
+from matplotlib import pyplot as plt
+import glob
+
+jc_files = glob.glob('bike-rental-data\data\JC-2016**-citibike-tripdata.csv')
+
+print(jc_files)
